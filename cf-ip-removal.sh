09eee46f79b191397f1fc304cfd6bf9b09392e0f
anonym0uz <102824282+anonymouz-trash@users.noreply.github.com>
2024-01-26 11:44:20 +0100

    Update cf-ip-removal.sh
    removed sensitive information

--- cf-ip-removal.sh
+++ cf-ip-removal.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-cf_ip_list=$(curl -s -X GET "https://api.cloudflare.com/client/v4/accounts/23632b407d6ad92b9540760fd47b7722/firewall/access_rules/rules?page=1&per_page=1000" -H "X-Auth-Email: thomasharnisch@tutanota.com" -H "X-Auth-Key: 6c3475330dcaa5495e1b4136ccd525409905b" -H "Content-Type: application/json" | jq -r '.result[] | .id')
 
+# Put your credentials here
+cf_zone_id=0
+cf_mail=0
+cf_global_api=0
+
+# Start of script
+cf_ip_list=$(curl -s -X GET "https://api.cloudflare.com/client/v4/accounts/${cf_zone_id}/firewall/access_rules/rules?page=1&per_page=1000" -H "X-Auth-Email: ${cf_mail}" -H "X-Auth-Key: ${cf_global_api}" -H "Content-Type: application/json" | jq -r '.result[] | .id')
 arr=($cf_ip_list)
 length=${#arr[@]}
-
 for (( i=0; i<${length};i++ ));
 do
   echo "item: $i/$length, value: ${arr[$i]}, deleted..."
-  curl -s -X DELETE "https://api.cloudflare.com/client/v4/accounts/23632b407d6ad92b9540760fd47b7722/firewall/access_rules/rules/${arr[$i]}" -H "X-Auth-Email: thomasharnisch@tutanota.com" -H "X-Auth-Key: 6c3475330dcaa5495e1b4136ccd525409905b" -H "Content-Type: application/json" >> /dev/null
+  curl -s -X DELETE "https://api.cloudflare.com/client/v4/accounts/${cf_zone_id}/firewall/access_rules/rules/${arr[$i]}" -H "X-Auth-Email: ${cf_mail}" -H "X-Auth-Key: ${cf_global_api}" -H "Content-Type: application/json" >> /dev/null
 done
